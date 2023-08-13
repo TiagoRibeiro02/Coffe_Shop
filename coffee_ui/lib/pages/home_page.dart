75e6fee77197b13cb96198599bc9639b8177d7c2
Tiago Ribeiro <95197303+TiagoRibeiro02@users.noreply.github.com>
2023-08-13 16:52:51 +0100

    base and botnavbar

---
+++ coffee_ui/lib/pages/home_page.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.notifications),
+              label: '',
+            )
+          ],
+        ),
+        body: Column(
+          children: [
+            //Find bast coffee for you
+
+            //Search bar
+
+            //horizontal listview of coffee tiles
+          ],
+        )
+    );
+  }
+}
