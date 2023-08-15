0388a6266d9d6334f10aaf1db1f222f5a2780423
Tiago Ribeiro <95197303+TiagoRibeiro02@users.noreply.github.com>
2023-08-15 19:19:09 +0100

    bottom nav bar

---
+++ coffee_shop/lib/pages/home_page.dart
@@ -0,0 +1,20 @@
+import 'package:coffee_shop/components/bottom_nav_bar.dart';
+import 'package:coffee_shop/const.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: backgroundColor,
+      bottomNavigationBar: MyBottomNavBar(),
+    );
+  }
+}
