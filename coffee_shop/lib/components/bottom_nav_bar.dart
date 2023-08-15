77940d8a48b990238f861b89f1bdba6b55b6dc1d
Tiago Ribeiro <95197303+TiagoRibeiro02@users.noreply.github.com>
2023-08-15 19:38:48 +0100

    pages change

--- coffee_shop/lib/components/bottom_nav_bar.dart
+++ coffee_shop/lib/components/bottom_nav_bar.dart
@@ -2,19 +2,31 @@ import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 
 class MyBottomNavBar extends StatelessWidget {
-  const MyBottomNavBar({super.key});
+  void Function(int)? onTabChange;
+  MyBottomNavBar({super.key, required this.onTabChange});
 
   @override
   Widget build(BuildContext context) {
-    return GNav(tabs: [
-      GButton(
-        icon: Icons.home,
-        text: 'shop',
-      ),
-      GButton(
-        icon: Icons.shopping_bag_outlined,
-        text: 'Cart',
-      ),
-    ]);
+    return Container(
+      margin: const EdgeInsets.all(25.0),
+      child: GNav(
+        onTabChange: (value) => onTabChange!(value),
+          color: Colors.grey[400],
+          mainAxisAlignment: MainAxisAlignment.center,
+          activeColor: Colors.grey[700],
+          tabBackgroundColor: Colors.grey.shade300,
+          tabBorderRadius: 24,
+          tabActiveBorder: Border.all(color: Colors.white),
+          tabs: const [
+            GButton(
+              icon: Icons.home,
+              text: 'shop',
+            ),
+            GButton(
+              icon: Icons.shopping_bag_outlined,
+              text: 'Cart',
+            ),
+      ]),
+    );
   }
 }
--- coffee_shop/lib/pages/home_page.dart
+++ coffee_shop/lib/pages/home_page.dart
@@ -1,6 +1,9 @@
 import 'package:coffee_shop/components/bottom_nav_bar.dart';
 import 'package:coffee_shop/const.dart';
+import 'package:coffee_shop/pages/shop_page.dart';
 import 'package:flutter/material.dart';
+
+import 'cart_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,11 +13,32 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  //Navigate bottom nav bar
+  int _selectedIndex=0;
+  void navigateBottomBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  //pages
+  final List<Widget> _pages = [
+    //shop page
+    ShopPage(),
+
+    //cart page
+    CartPage(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: backgroundColor,
-      bottomNavigationBar: MyBottomNavBar(),
+      bottomNavigationBar: MyBottomNavBar(
+        onTabChange: (index) => navigateBottomBar(index),
+      ),
+      body: _pages[_selectedIndex],
     );
   }
 }
